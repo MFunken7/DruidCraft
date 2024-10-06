b58dcc185f033cfd52617bc1437fe4a406355139
Michael <114317916+MFunken7@users.noreply.github.com>
2024-10-05 21:17:08 -0600

    Finished camera zoom

--- DruidCraft/Assets/Scripts/CameraControl.cs
+++ DruidCraft/Assets/Scripts/CameraControl.cs
@@ -8,23 +8,49 @@ public class CameraControl : MonoBehaviour
     [SerializeField, Range(50, 70)] int MaxRotation = 60;
 	[SerializeField] Camera camera;
 
-	float rotationAngle = 2;
+	float rotationAngle = -2;
 
 	void Update()
 	{
+		if(Input.mouseScrollDelta.y != 0)
+		{
+			Zoom(Input.mouseScrollDelta.y);
+		}
+
+	}
+
+	private void Zoom(float zoomValue)
+	{
 		Vector3 zoom = Vector3.zero;
 
-		zoom.y = Input.mouseScrollDelta.y;
+		zoom.y = -zoomValue;
 
 		camera.transform.localPosition += zoom;
 
 		float cameraY = camera.transform.localPosition.y;
-		
+
 		cameraY = Mathf.Clamp(cameraY, 10, MaxHeight);
 
 		Vector3 finalPosition = new Vector3(camera.transform.localPosition.x, cameraY, camera.transform.localPosition.z);
 
 		camera.transform.localPosition = finalPosition;
+
+		float currentRotation = camera.transform.localRotation.eulerAngles.x;
+		Debug.Log("currentRotation - " + currentRotation);
+
+		float newRotation = currentRotation + (rotationAngle * zoomValue);
+
+		newRotation = Mathf.Clamp(newRotation, 50, MaxRotation);
+
+		Debug.Log("postClamp - " + newRotation);
+
+		Quaternion rotation = Quaternion.Euler(newRotation, 0, 0);
+
+
+		camera.transform.localRotation = rotation;
+
+
+
 
 	}
 }
